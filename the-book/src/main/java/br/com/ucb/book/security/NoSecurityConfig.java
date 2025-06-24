61cec64db0847e8d75ab3593d5670a124c147f54
Guilherme Corá <guilhermearcora@gmail.com>
2025-06-23 21:15:56 -0300

    Cors

--- the-book/src/main/java/br/com/ucb/book/security/NoSecurityConfig.java
+++ the-book/src/main/java/br/com/ucb/book/security/NoSecurityConfig.java
@@ -5,12 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class NoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(cors ->
+                cors.configurationSource(request -> corsConfiguration())
+        );
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/permitted/**")
@@ -19,4 +28,14 @@ public class NoSecurityConfig {
                 .securityMatcher("/permitted/**");
         return http.build();
     }
+
+    @Bean
+    CorsConfiguration corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        return configuration;
+    }
+
 }
